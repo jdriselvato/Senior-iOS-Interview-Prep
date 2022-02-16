590bcb5ae118fd33c482bb0a35b84f13d1a039a6
jdriselvato <jdriselvato@gmail.com>
2022-02-16 09:51:40 -0800

    Added a second attempt for later

---
+++ {ik} UpLevel/week 2/Sorting Algorithms Practice - New Problem Set 1/Merge One Sorted Array Into Another - Attempt 2.swift
@@ -0,0 +1,32 @@
+// Merge One Sorted Array Into Another
+
+/*First array has n positive numbers, and they are sorted in the non-descending order.
+
+Second array has 2n numbers: first n are also positive and sorted in the same way but the last n are all zeroes.
+
+Merge the first array into the second and return the latter. You should get 2n positive integers sorted in the non-descending order.
+
+Example:
+
+{
+"first": [1, 3, 5],
+"second": [2, 4, 6, 0, 0, 0]
+}
+
+Output: [1, 2, 3, 4, 5, 6]
+
+Notes
+
+Constraints:
+
+1 <= n <= 105
+1 <= array elements (except those zeroes) <= 2 * 109
+You can use only constant auxiliary space
+*/
+
+// Try it this time without combining the array since the data is already split
+
+func merge_one_into_another(first: [Int], second: [Int]) -> [Int] {
+        
+    return []
+}
