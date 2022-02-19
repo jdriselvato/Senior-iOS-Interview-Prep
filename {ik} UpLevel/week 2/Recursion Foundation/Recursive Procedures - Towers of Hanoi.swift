aa00173d71c3114ac25e5d46a6097e27b4df14b5
jdriselvato <jdriselvato@gmail.com>
2022-02-19 11:56:02 -0800

    Towers of Hanoi

--- {ik} UpLevel/week 2/Recursion Foundation/Recursive Procedures - Towers of Hanoi.swift
+++ {ik} UpLevel/week 2/Recursion Foundation/Recursive Procedures - Towers of Hanoi.swift
@@ -43,19 +43,33 @@ Finish:
 
 // Video's psuedo code
 
-func towerOfHanoi(n: Int, src: String, dest: String, aux: String) {
-	if n == 1 {
-		print("\tMove disc from", src, "to", dest)
-		return
+func Helper(n: Int, src: String, dest: String, aux: String) -> Int {
+
+	var count = 0
+
+	func towerOfHanoi(n: Int, src: String, dest: String, aux: String) { // O(2^n) or Exponential time
+		if n == 1 {
+			// print("\tMove disc from", src, "to", dest)
+			count += 1
+			return
+		}
+
+		towerOfHanoi(n: n-1, src: src, dest: aux, aux: dest)
+		// print("\tMove disc from", src, "to", dest)
+		count += 1
+		towerOfHanoi(n: n-1, src: aux, dest: dest, aux: src)
 	}
 
-	towerOfHanoi(n: n-1, src: src, dest: aux, aux: dest)
-	print("\tMove disc from", src, "to", dest)
-	towerOfHanoi(n: n-1, src: aux, dest: dest, aux: src)
+	towerOfHanoi(n: n, src: src, dest: dest, aux: aux)
+
+	return count
 }
 
 print("Instructions with 3 discs:")
-towerOfHanoi(n: 3, src: "A", dest: "B", aux: "C")
+_ = Helper(n: 3, src: "A", dest: "B", aux: "C")
 
-print("Instructions with 100 discs:")
-towerOfHanoi(n: 100, src: "A", dest: "B", aux: "C")
+print("Instructions with 5 discs:")
+let count = Helper(n: 8, src: "A", dest: "B", aux: "C")
+print("Took", count, "moves")
+
+// This is an example of depth first because we go down a single lane (left) first then it's right 
